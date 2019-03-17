4d9224020add773515c64ed31de5164ae6e9aa68
Ravikumar M <rmittapa@uncc.edu>
2019-03-16 21:14:01 -0400

    Modified code for all codes

--- 5.assignment-openmp-advanced/5.assignment-openmp-advanced/lcs/lcs.cpp
+++ 5.assignment-openmp-advanced/5.assignment-openmp-advanced/lcs/lcs.cpp
@@ -1,10 +1,15 @@
+#include <omp.h>
 #include <stdio.h>
+#include <iostream>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <iostream>
 #include <unistd.h>
-
+#include <algorithm>
+#include <math.h>
+#include <stdlib.h>
+#include <chrono>
+#include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -18,11 +23,62 @@ extern "C" {
 #endif
 
 
+/* Utility function to get max of 2 integers */
+int max(int a, int b) 
+{ 
+  return (a > b)? a : b; 
+} 
+
+/* Returns length of LCS for X[0..m-1], Y[0..n-1] */
+int lcs( char *X, char *Y, int m, int n, int nbthreads ) 
+{ 
+  int granularity = 500;
+  if(n<=10)
+    granularity = 50;
+  else 
+    granularity = 5*n*0.01;
+  
+  int** C = new int*[m+1];
+  for (int i=0; i<=m; ++i) {
+    C[i] = new int[n+1];
+  }
+  
+  int i, j; 
+  
+  #pragma omp parallel for schedule(guided,granularity)
+  for (i=0; i<=m; i++) 
+  { 
+    for (j=0; j<=n; j++) 
+    { 
+      if (i == 0 || j == 0) 
+        C[i][j] = 0; 
+
+      else if (X[i-1] == Y[j-1]) 
+        C[i][j] = C[i-1][j-1] + 1; 
+
+      else
+        C[i][j] = max(C[i-1][j], C[i][j-1]); 
+    } 
+  } 
+
+  int result = C[m][n];
+  
+  #pragma omp taskwait
+  for (int i=0; i<=m; ++i) { 
+    delete[] C[i];
+  }
+  delete[] C;
+  
+  return result; 
+  
+} 
+
+
 
 int main (int argc, char* argv[]) {
 
-  //forces openmp to create the threads beforehand
-#pragma omp parallel
+//forces openmp to create the threads beforehand
+  #pragma omp parallel
   {
     int fd = open (argv[0], O_RDONLY);
     if (fd != -1) {
@@ -37,6 +93,9 @@ int main (int argc, char* argv[]) {
     return -1;
   }
 
+  int nbthreads = atoi(argv[3]);
+  omp_set_num_threads(nbthreads);
+  
   int m = atoi(argv[1]);
   int n = atoi(argv[2]);
 
@@ -46,12 +105,22 @@ int main (int argc, char* argv[]) {
   generateLCS(X, m, Y, n);
 
   
-  //insert LCS code here.
-  int result = -1; // length of common subsequence
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 
+  int lcs_res = 0;
+  
+  #pragma omp parallel
+  #pragma omp single nowait
+  lcs_res = lcs( X, Y, m, n , nbthreads) ;
+  
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elpased_seconds = end-start;
 
-  checkLCS(X, m, Y, n, result);
+  checkLCS(X, m, Y, n, lcs_res);
+  std::cerr<<elpased_seconds.count()<<std::endl;
 
+  delete[] X;
+  delete[] Y;
 
   return 0;
 }
--- 5.assignment-openmp-advanced/5.assignment-openmp-advanced/mergesort/mergesort.cpp
+++ 5.assignment-openmp-advanced/5.assignment-openmp-advanced/mergesort/mergesort.cpp
@@ -1,9 +1,14 @@
+#include <omp.h>
 #include <stdio.h>
+#include <iostream>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <iostream>
 #include <unistd.h>
+#include <algorithm>
+#include <chrono>
+
+#define DEBUG 0
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,16 +16,108 @@ extern "C" {
 
   void generateMergeSortData (int* arr, size_t n);
   void checkMergeSortResult (int* arr, size_t n);
-
+  
 #ifdef __cplusplus
 }
 #endif
+
+void merge(int * arr, int l, int mid, int r, int * temp) {
+
+  #if DEBUG
+    std::cout<<l<<" "<<mid<<" "<<r<<std::endl;
+  #endif
+
+  // short circuits
+  if (l == r) return;
+  if (r-l == 1) {
+    if (arr[l] > arr[r]) {
+      int temp = arr[l];
+      arr[l] = arr[r];
+      arr[r] = temp;
+    }
+    return;
+  }
+
+  int i, j, k;
+  int n = mid - l;
+
+  // declare and init temp arrays
+  //int *temp = new int[n];
+  for (i=0; i<n; ++i)
+  temp[l+i] = arr[l+i];
+
+  i = 0;    // temp left half
+  j = mid;  // right half
+  k = l;    // write to 
+
+  // merge
+  while (i<n && j<=r) {
+    if (temp[l+i] <= arr[j] ) {
+      arr[k++] = temp[l+i];
+      i++;
+    } else {
+      arr[k++] = arr[j++];
+    }
+  }
+
+  // exhaust temp 
+  while (i<n) {
+    arr[k++] = temp[l+i];
+    i++;
+  }
+
+}
+
+
+void mergesort(int * arr, int l, int r, int n, int * temp) {
+  long s = 50;
+  
+  if(n<=10)
+    s = 50;
+  else if(n<1000 && n>=100)
+    s = 5*n*0.1;
+  else if(n>=1000 && n < 100000)
+    s = 5*n*0.01;
+  else if(n==100000)
+    s = 5*n*0.01;
+  else if( n==1000000)
+    s = 5*n*0.001;
+  else if( n==10000000)
+    s = 5*n*0.0001;
+  else 
+    s = 5000;
+  
+  //#pragma omp parallel
+  if (r-l > s) {
+    int mid = (l+r)/2;
+    
+    #pragma omp task
+    mergesort(arr, l, mid, n, temp);
+    
+    #pragma omp task
+    mergesort(arr, mid+1, r, n, temp);
+    
+    #pragma omp taskwait
+    merge(arr, l, mid+1, r, temp);
+  }
+  else
+  {
+    if (l < r) {
+        int mid = (l+r)/2;
+        mergesort(arr, l, mid, n, temp);
+        mergesort(arr, mid+1, r, n, temp);
+        merge(arr, l, mid+1, r,temp);
+    }
+  }
+}
+
+
 
 
 int main (int argc, char* argv[]) {
 
   //forces openmp to create the threads beforehand
-#pragma omp parallel
+  #pragma omp parallel
   {
     int fd = open (argv[0], O_RDONLY);
     if (fd != -1) {
@@ -30,24 +127,57 @@ int main (int argc, char* argv[]) {
       std::cerr<<"something is amiss"<<std::endl;
     }
   }
-  
+
   if (argc < 3) { std::cerr<<"usage: "<<argv[0]<<" <n> <nbthreads>"<<std::endl;
     return -1;
   }
 
-  int n = atoi(argv[1]);
+
+  // command line parameter
   
+  int nbthreads = atoi(argv[2]);
+  omp_set_num_threads(nbthreads);
+  long n;
+  n = atol(argv[1]);
+
   // get arr data
   int * arr = new int [n];
+  int * temp = new int [n];
   generateMergeSortData (arr, n);
 
-  //insert sorting code here.
+  #if DEBUG
+  for (int i=0; i<n; ++i) 
+    std::cout<<arr[i]<<" ";
+    std::cout<<std::endl;
+  #endif
 
+  // begin timing
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
 
-  
+  // sort
+  #pragma omp parallel
+  {
+    #pragma omp single
+    {
+      mergesort(arr, 0, n-1, n, temp );
+    }
+  }
+  // end timing
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elpased_seconds = end-start;
+
+  // display time to cerr
+  std::cerr<<elpased_seconds.count()<<std::endl;
   checkMergeSortResult (arr, n);
-  
+
+  #if DEBUG
+  for (int i=0; i<n; ++i) 
+    std::cout<<arr[i]<<" ";
+    std::cout<<std::endl;
+  #endif
+
   delete[] arr;
+  delete[] temp;
 
   return 0;
 }
