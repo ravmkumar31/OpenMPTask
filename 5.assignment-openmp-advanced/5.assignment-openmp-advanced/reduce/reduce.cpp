8677e5edd0a653e3e94e50fb999e28cc060a2269
Ravikumar M <rmittapa@uncc.edu>
2019-03-16 23:10:24 -0400

    Modified reduce code

--- 5.assignment-openmp-advanced/5.assignment-openmp-advanced/reduce/reduce.cpp
+++ 5.assignment-openmp-advanced/5.assignment-openmp-advanced/reduce/reduce.cpp
@@ -4,8 +4,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include<chrono>
 #include <unistd.h>
-#include <chrono>
 
 
 #ifdef __cplusplus
@@ -18,12 +18,9 @@ extern "C" {
 }
 #endif
 
-int *th_sum;
-void reduce(int last,int *arr,int granularity,long int n);
 
 
 int main (int argc, char* argv[]) {
-
   //forces openmp to create the threads beforehand
 #pragma omp parallel
   {
@@ -35,64 +32,55 @@ int main (int argc, char* argv[]) {
       std::cerr<<"something is amiss"<<std::endl;
     }
   }
-
-  if (argc < 4) {
-    std::cerr<<"Usage: "<<argv[0]<<" <n> <nbthreads> <granularity>"<<std::endl;
+  
+  if (argc < 3) {
+    std::cerr<<"usage: "<<argv[0]<<" <n> <nbthreads>"<<std::endl;
     return -1;
   }
 
-  int * arr = new int [atoi(argv[1])];
+
+  int n = atoi(argv[1]);
+  int nbthreads= atoi(argv[2]);
+  int chunk = n/nbthreads;
+  int * arr = new int [n];
+  int reslt=0,i,tid;
 
   generateReduceData (arr, atoi(argv[1]));
 
-  //write code here
-  long int n = atol (argv[1]);
-  int nbthreads = atoi (argv[2]);
-  int granularity = atoi (argv[3]);
-  th_sum = new int[nbthreads+1];
-
-  omp_set_num_threads(nbthreads);
-  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
-  #pragma omp parallel
+  //insert reduction code here
+  auto clock_start = std::chrono::system_clock::now(); 
+  
+  #pragma omp parallel default(shared) private(i, tid)
   {
-
-    #pragma omp single
-    {
-      int last = 0;
-      while(last < n){
-        //std::cout<<last<<" ";
-        #pragma omp task
-        {
-            reduce(last,arr,granularity,n);
-        }
-        last+=granularity;
-      }
-      #pragma omp taskwait
-
+     int begin, end, sum=0;
+           tid= omp_get_thread_num();
+           begin= ((tid)*chunk);
+           end= ((tid+1)*chunk);
+  
+            #pragma omp task
+            {
+                 for(i=begin; i<end; i++){
+      sum+= arr[i] ;
     }
+           }
+            #pragma omp critical
+              reslt+=sum; 
   }
-  int result;
-  for(int i=0;i<nbthreads;i++){
-        result = result + th_sum[i];
-  }
-  std::cout<<result<<std::endl;
-  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
-  std::chrono::duration<double> elapsed_seconds = end-start;
+  
+
+  auto clock_end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = clock_end-clock_start;
+
+  std::cout<<reslt<<std::endl;
+
   std::cerr<<elapsed_seconds.count()<<std::endl;
+    
 
   delete[] arr;
 
   return 0;
 }
-void reduce(int last,int *arr,int granularity,long int n) {
-    int t_no = omp_get_thread_num();
-    int sum = th_sum[t_no];
-    int end = last + granularity;
-    if(end>n){
-        end = n;
-    }
-    for(int j = last;j < end; j++){
-        sum += arr[j];
-    }
-    th_sum[t_no] = sum;
-}
+
+  
+  
+  
